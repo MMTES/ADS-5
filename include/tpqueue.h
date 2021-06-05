d17632974b7e907fd0e6646e37badaef1d807634
MMTES <42440704+MMTES@users.noreply.github.com>
2021-06-05 23:18:48 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,29 +4,72 @@
 #include <cassert>
 
 template<typename T>
-class TPQueue { 
- public: 
-  TPQueue() :tail(nullptr), head(nullptr) {}
-  ~TPQueue() {
-    while (head)
-      pop();
-  }
+class TPQueue {
+    struct ITEM {
+        T data;
+        ITEM *next;
+    };
  private:
-  struct ITEM { 
-    T data;
-    ITEM* next;
-    ITEM* prev;
-  };
-  ITEM* head;
-  ITEM* tail;
-  TPQueue::ITEM* create(const T& data, ITEM*prev) {
-    item->prev = prev;
+    TPQueue::ITEM *create(const T &);
+    ITEM *tail;
+    ITEM *head;
+ public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    ~TPQueue();
+    void push(const T &);
+    T pop();
+};
+
+template<typename T>
+typename TPQueue<T>::ITEM *TPQueue<T>::create(const T &data) {
+    ITEM *item = new ITEM;
     item->data = data;
-    ITEM* item = new ITEM;
     item->next = nullptr;
     return item;
-  }
-};
+}
+template<typename T>
+TPQueue<T>::~TPQueue() {
+    while (head)
+        pop();
+}
+template<typename T>
+void TPQueue<T>::push(const T &data) {
+    if (tail && head) {
+        ITEM *temp = head;
+        if (temp->data.prior < data.prior) {
+            temp = create(data);
+            temp->next = head;
+            head = temp;
+        } else {
+            while (temp->next) {
+                if ( temp->next->data.prior < data.prior) {
+                    ITEM *present = create(data);
+                    present->next = temp->next;
+                    temp->next = present;
+                    break;
+                } else {
+                    temp = temp->next;
+                }
+            }
+        }
+        if (!temp->next) {
+            tail->next = create(data);
+            tail = tail->next;
+        }
+    } else {
+        head = create(data);
+        tail = head;
+    }
+}
+template<typename T>
+T TPQueue<T>::pop() {
+    if (head) {
+        ITEM *temp = head->next;
+        T data = head->data;
+        delete head;
+        head = temp;
+        return data;
+    }
 
 struct SYM {
   char ch;
